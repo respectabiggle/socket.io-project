8e158ad17bd376a7a8a255c9633c33db39f68e75
respectabiggle <55676087+respectabiggle@users.noreply.github.com>
2022-10-12 23:12:01 -0400

    added "nocomment" version of server.js

---
+++ servernocomments.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app); 
+server.listen(3511, () => console.log("Express running! on port 3511")); 
+app.use(express.static('public'));
+
+
+
+const { Server } = require('socket.io'); 
+const io = new Server(server);         
+
+
+    // io.on('connection', (socket) => { 
+    //         console.log('a user connected');
+    //         socket.on('disconnect', () => {
+    //             console.log('user disconnected');
+    //         });
+    //         socket.on('hello test', (msg) => {
+    //             console.log('message: ' + msg);
+    //         });
+    //         socket.emit('hello test', 'Hi from Server :))');      
+    // });
+
+
+const { WebSocket } = require('ws');
+const externalClient = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade');
+
+    let data;
+    let externalDataInAnObject;  
+    let exampleVariable = 11;
+
+    externalClient.on('message', (event) => {
+
+    externalData = (event); 
+    externalDataParsed = JSON.parse(event); 
+    externalDataExtractedValue = parseFloat(externalDataParsed.p)
+    externalDataExtractedValueFixed = parseFloat(externalDataParsed.p).toFixed(2); 
+    externalDataExtractedValueFixedAsFloat = parseFloat(externalDataExtractedValueFixed);    
+    data = externalDataExtractedValueFixedAsFloat;
+
+    io.emit('numberChannel', data);                              
+
+        // letexternalDataInAnObject = { data, exampleVariable }; 
+        // io.emit('objectChannel', externalDataInAnObject); 
+
+    });
+
+
+        // const sendDataToClient = function (){  
+        //     io.emit('numberChannel', data);
+        // };
+        // setInterval(sendDataToClient, 500);
